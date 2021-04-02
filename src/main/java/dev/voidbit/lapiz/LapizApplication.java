9903aeaf3d7d0dbdf171769d68488a62e96cf5dc
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-04-02 18:44:04 +0200

    Create Spring Boot project

---
+++ src/main/java/dev/voidbit/lapiz/LapizApplication.java
@@ -0,0 +1,13 @@
+package dev.voidbit.lapiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LapizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/voidbit/lapiz/LapizApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.voidbit.lapiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

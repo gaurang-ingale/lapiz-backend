7d9c498f1d3666b5aa0f688d94c40d68e090f31c
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-04-04 16:16:47 +0200

    write failing test for SubjectRepository

---
+++ src/test/java/dev/voidbit/lapiz/SubjectRepositoryTest.java
@@ -0,0 +1,35 @@
+package dev.voidbit.lapiz;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class SubjectRepositoryTest {
+
+    @Autowired
+    private SubjectRepository subjectRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void getSubjectById_returnsSubject() throws Exception{
+        //arrange
+        Subject subject = testEntityManager.persistFlushFind(new Subject(1L, "Computer Science"));
+        testEntityManager.detach(subject);
+
+        //act
+        Subject response = subjectRepository.getSubjectById(1L);
+
+        //assert
+        assertThat(response.getName()).isEqualTo("Computer Science");
+        assertThat(response.getId()).isEqualTo(1L);
+    }
+}

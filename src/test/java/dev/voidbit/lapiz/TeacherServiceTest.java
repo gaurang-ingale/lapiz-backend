c579f83ac705484e71d59f06c5351389f17f9a24
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-04-09 18:36:36 +0200

    write failing tests for TeacherService

---
+++ src/test/java/dev/voidbit/lapiz/TeacherServiceTest.java
@@ -0,0 +1,45 @@
+package dev.voidbit.lapiz;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class TeacherServiceTest {
+    @Mock
+    private TeacherRepository teacherRepository;
+
+    private TeacherService teacherService;
+
+    @BeforeEach
+    private void beforeEach(){
+        teacherService = new TeacherService(this.teacherRepository);
+    }
+
+    @Test
+    public void getTeacherById_returnsTeacher() throws Exception{
+        //arrange
+        given(teacherRepository.getTeacherById(anyLong()))
+                .willReturn(new Teacher(1L, "Merlin Magic"));
+        //act
+        Teacher teacher = teacherService.getTeacherById(1L);
+        //assert
+        Assertions.assertThat(teacher).isNotNull();
+        Assertions.assertThat(teacher.getName()).isEqualTo("Merlin Magic");
+        Assertions.assertThat(teacher.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    public void getTeacherById_notFound(){
+        //act
+        Teacher teacher = teacherService.getTeacherById(1L);
+        //assert
+        Assertions.assertThat(teacher).isNull();
+    }
+}

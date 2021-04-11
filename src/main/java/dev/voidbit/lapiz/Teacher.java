38b17ad0c48088c9504952275ec6f382fcbacb9b
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-04-11 17:42:37 +0200

    make Teacher pass its unit tests

--- src/main/java/dev/voidbit/lapiz/Teacher.java
+++ src/main/java/dev/voidbit/lapiz/Teacher.java
@@ -22,23 +22,37 @@ public class Teacher {
     private String firstName;
     private String lastName;
 
+    private void composeNameFromFirstAndLastNames(){
+        this.name = this.firstName + " " + this.lastName;
+    }
+
+    private void seperateFirstAndLastNamesFromName(){
+        String[] names = name.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+    }
+
     public Teacher(String name) {
         this.name = name;
+        seperateFirstAndLastNamesFromName();
     }
 
     public Teacher(Long id, String name){
         this.id = id;
         this.name = name;
+        seperateFirstAndLastNamesFromName();
     }
 
     public Teacher(Long id, String firstName, String lastName){
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
+        composeNameFromFirstAndLastNames();
     }
 
     public Teacher(String firstName, String lastName){
         this.firstName = firstName;
         this.lastName = lastName;
+        composeNameFromFirstAndLastNames();
     }
 }

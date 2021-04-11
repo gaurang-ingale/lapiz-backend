0971f86ea19f64c98926fabd570dc946a0673628
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-04-11 17:46:07 +0200

    write more failing unit tests for Student

--- src/test/java/dev/voidbit/lapiz/StudentTest.java
+++ src/test/java/dev/voidbit/lapiz/StudentTest.java
@@ -15,4 +15,25 @@ public class StudentTest {
         Assertions.assertThat(Student.class).hasDeclaredFields("lastName");
         Assertions.assertThat(new Student("some", "thing").getLastName()).hasSameClassAs(new String());
     }
+
+    @Test
+    public void firstName_isTheFirstSliceOfThe_name() throws Exception{
+        Student student = new Student("Abra Cadabra");
+        Assertions.assertThat(student.getFirstName()).isNotNull();
+        Assertions.assertThat(student.getFirstName()).isEqualTo("Abra");
+    }
+
+    @Test
+    public void lastName_isTheLastSliceOfThe_name() throws Exception{
+        Student student = new Student("Abra Cadabra Simsalabim");
+        Assertions.assertThat(student.getLastName()).isNotNull();
+        Assertions.assertThat(student.getLastName()).isEqualTo("Simsalabim");
+    }
+
+    @Test
+    public void name_is_lastNameAppendedToFirstNameWithASpace() throws Exception{
+        Student student = new Student("Abra", "Cadabra");
+        Assertions.assertThat(student.getName()).isNotNull();
+        Assertions.assertThat(student.getName()).isEqualTo("Merlin Magic");
+    }
 }

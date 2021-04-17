e7823b49375fc53d83c4a707fe1ebd56c5a22c01
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-04-17 20:22:25 +0200

    write failing unit tests for subject

---
+++ src/test/java/dev/voidbit/lapiz/SubjectTest.java
@@ -0,0 +1,33 @@
+package dev.voidbit.lapiz;
+
+import org.assertj.core.api.Assert;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class SubjectTest {
+    @Test
+    public void subject_hasAName() throws Exception{
+        Assertions.assertThat(Subject.class).hasDeclaredFields("name");
+        Assertions.assertThat(new Subject().getName()).hasSameClassAs(new String());
+    }
+
+    @Test
+    public void subject_hasTeachers() throws Exception{
+        Assertions.assertThat(Subject.class).hasDeclaredFields("teachers");
+        Assertions.assertThat(new Subject().getTeachers()).hasSameClassAs(new ArrayList<Teacher>());
+    }
+
+    @Test
+    public void subject_hasStudents() throws Exception{
+        Assertions.assertThat(Subject.class).hasDeclaredFields("students");
+        Assertions.assertThat(new Subject().getStudents()).hasSameClassAs(new ArrayList<Student>());
+    }
+
+    @Test
+    public void subject_hasADescription() throws Exception {
+        Assertions.assertThat(Subject.class).hasDeclaredFields("description");
+        Assertions.assertThat(new Subject().getDescription()).hasSameClassAs(new String());
+    }
+}

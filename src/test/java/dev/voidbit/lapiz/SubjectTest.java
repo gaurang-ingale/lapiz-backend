693b86252957893a50b1ef95630e97c49810d0b1
Gaurang Ingale <gaurang.ingale@outlook.com>
2021-05-05 22:10:02 +0200

    write failing unit test for subject

--- src/test/java/dev/voidbit/lapiz/SubjectTest.java
+++ src/test/java/dev/voidbit/lapiz/SubjectTest.java
@@ -28,6 +28,13 @@ public class SubjectTest {
     @Test
     public void subject_hasADescription() throws Exception {
         Assertions.assertThat(Subject.class).hasDeclaredFields("description");
-        Assertions.assertThat(new Subject("Computer Science", "The study of computers and mathematics").getDescription()).hasSameClassAs(new String());
+        Assertions.assertThat(new Subject("Computer Science", "The study of computers and mathematics")
+                .getDescription()).hasSameClassAs(new String());
+    }
+
+    @Test
+    public void subject_hasAListOfTopics() throws Exception{
+        Assertions.assertThat(Subject.class).hasDeclaredFields("topics");
+        Assertions.assertThat(new Subject().getTopics()).hasSameClassAs(new ArrayList<Topic>());
     }
 }
